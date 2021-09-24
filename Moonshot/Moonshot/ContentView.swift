d405c041ab01161151b145122c1748967f9302e7
Mariana Yamamoto <myamamot@corp.lifescan.com>
2021-09-24 14:24:37 -0400

    add geometry reader to fit image to geo size

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image("Example")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 300, height: 300)
+            GeometryReader { geo in
+                Image("Example")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geo.size.width)
+            }
         }
     }
 }
