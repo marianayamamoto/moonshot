baa10beda00bedbc35738a943bc2b5cf4914b1af
Mariana Yamamoto <myamamot@corp.lifescan.com>
2021-09-24 14:53:51 -0400

    navigation link

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct CustomText: View {
     var text: String
-
+    
     var body: some View {
         Text(text)
     }
-
+    
     init(_ text: String) {
         print("Creating a new CustomText")
         self.text = text
@@ -22,15 +22,16 @@ struct CustomText: View {
 
 struct ContentView: View {
     var body: some View {
-        List {
-            ForEach(0..<100) {
-                CustomText("Item \($0)")
-                        .font(.title)
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
+                }
             }
+            .navigationTitle("SwiftUI")
         }
-        .frame(maxWidth: .infinity)
     }
-
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
