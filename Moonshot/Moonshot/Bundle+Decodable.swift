6029e931c3876680bd11a91b37d92818fc3e95ff
Mariana Yamamoto <myamamot@corp.lifescan.com>
2021-09-28 10:08:14 -0400

    show list of missions

--- Moonshot/Moonshot/Bundle+Decodable.swift
+++ Moonshot/Moonshot/Bundle+Decodable.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file: String) -> [Astronaut] {
+    func decode<T: Codable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
             fatalError("Failed to locate \(file) in bundle.")
         }
@@ -18,8 +18,11 @@ extension Bundle {
         }
 
         let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
 
-        guard let loaded = try? decoder.decode([Astronaut].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle.")
         }
 
